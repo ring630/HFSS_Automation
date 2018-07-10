b6dc1b3c8966dcde2ab0b7962f77750bfbc6999d
ehuizou <hui.zhou@ericsson.com>
2018-07-10 20:41:16 +0200

    the first version

---
+++ single_via.py
@@ -0,0 +1,10 @@
+from util import HFSS
+
+model_dx = "2mm"
+model_dy = "2mm"
+
+
+
+app = HFSS()
+app.new_project()
+app.new_hfss_design("test_design")
---
+++ util/hfss_script.py
@@ -0,0 +1,85 @@
+import win32com.client
+import numpy as np
+
+class HFSS:
+    def __init__(self):
+        app = win32com.client.Dispatch("Ansoft.ElectronicsDesktop")
+        self.oDesktop = app.GetAppDesktop()
+
+        self.oProject = None
+        self.oDesign = None
+
+    def new_project(self):
+
+        self.oProject = self.oDesktop.NewProject()
+
+    def new_hfss_design(self, name, solution="Terminal"):
+
+        self.oProject.InsertDesign("HFSS", name, solution, "")
+        self.oDesign = self.oProject.SetActiveDesign(name)
+
+
+    def create_conductor(self, x, y, z, dx, dy, dz, name):
+        oEditor = self.oDesign.SetActiveEditor("3D Modeler")
+        oEditor.CreateBox(
+            [
+                "NAME:BoxParameters",
+                "XPosition:="	, x,
+                "YPosition:="		, y,
+                "ZPosition:="		, z,
+                "XSize:="		, dx,
+                "YSize:="		, dy,
+                "ZSize:="		, dz
+            ],
+            [
+                "NAME:Attributes",
+                "Name:="		, name,
+                "Flags:="		, "",
+                "Color:="		, "(255 128 64)",
+                "Transparency:="	, 0,
+                "PartCoordinateSystem:=", "Global",
+                "UDMId:="		, "",
+                "MaterialValue:="	, "\"pec\"",
+                "SurfaceMaterialValue:=", "\"\"",
+                "SolveInside:="		, False,
+                "IsMaterialEditable:="	, True,
+                "UseMaterialAppearance:=", False
+            ])
+
+    def create_dielectric(self, x, y, z, radius, height, axis, name):
+        oEditor = self.oDesign.SetActiveEditor("3D Modeler")
+        oEditor.CreateCylinder(
+            [
+                "NAME:CylinderParameters",
+                "XCenter:="	, x,
+                "YCenter:="		, y,
+                "ZCenter:="		, z,
+                "Radius:="		, radius,
+                "Height:="		, height,
+                "WhichAxis:="		, axis,
+                "NumSides:="		, "0"
+            ],
+            [
+                "NAME:Attributes",
+                "Name:="		, name,
+                "Flags:="		, "",
+                "Color:="		, "(0 128 0)",
+                "Transparency:="	, 0.8,
+                "PartCoordinateSystem:=", "Global",
+                "UDMId:="		, "",
+                "MaterialValue:="	, "\"FR4_epoxy\"",
+                "SurfaceMaterialValue:=", "\"\"",
+                "SolveInside:="		, True,
+                "IsMaterialEditable:="	, True,
+                "UseMaterialAppearance:=", False
+            ])
+
+def debug():
+    app = HFSS()
+    app.new_project()
+    app.new_hfss_design("test_design")
+    app.create_conductor("0mm", "0mm", "0mm", "1mm", "1mm", "1mm", "testbox")
+    app.create_dielectric("0mm", "0mm", "0mm", "1mm", "1mm", "Z", "testbox")
+
+if __name__ == '__main__':
+    debug()
